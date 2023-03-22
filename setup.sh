e38b305d9607b7b24fe032fb0cfff297754dc427
Noah Franz WSL <noahfranz13@gmail.com>
2023-03-22 16:40:44 -0400

    add setup file

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# bash file to setup environment variables
+
+# create environment variables to query the OpenStreetMap API
+# YOU SHOULD EDIT THESE BEFORE RUNNING ANYTHING
+export OSM_USR='yourusername'
+export OSM_PWD='yourpassword'
