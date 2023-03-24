86ee34360d4b2e2437adb7e4414b82229217a603
Noah Franz WSL <noahfranz13@gmail.com>
2023-03-23 23:36:38 -0400

    query with overpass

--- utilities/getData.py
+++ utilities/getData.py
@@ -5,25 +5,17 @@ for this project
 
 import os
 import overpy
-from subprocess import run
-import pandas as pd
 
 # hard code in boundary coordinates for now
 llLat = 42.5609 # lower left latitude
 llLon = -74.0396 # lower left longitude
-urLat = 43.0609 # upper right latitude
-urLon = -73.5396 # upper right longitude
+urLat = 42.8#43.0609 # upper right latitude
+urLon = -73.8#-73.5396 # upper right longitude
 
 outfile = os.path.join(os.getcwd(),'test.xml')
 
-if not os.path.isfile(outfile):
-    # download the data using wget in a subprocess
-    url=f"http://overpass-api.de/api/interpreter?data=(node({llLat},{llLon},{urLat},{urLon});<;rel(br););out meta;"
-    cmd = f'wget -O {outfile} "{url}"'
-    run(cmd, shell=True)
+query = f'[out:json];node({llLat},{llLon},{urLat},{urLon});out;'
 
-# use the overpass API
-df = pd.read_xml(outfile)
-
-# start using overpy
-result = overpy.Result().from_xml(df.to_string())
+api = overpy.Overpass()
+result = api.query(query)
+print(result)
