8a69b4d49dbe200965010c99c656e8d378c18109
Noah Franz WSL <noahfranz13@gmail.com>
2023-03-24 00:34:11 -0400

    update output data path for org

--- util/getData.py
+++ util/getData.py
@@ -12,13 +12,7 @@ llLon = -74.0396 # lower left longitude
 urLat = 42.8#43.0609 # upper right latitude
 urLon = -73.8#-73.5396 # upper right longitude
 
-outfile = os.path.join(os.getcwd(),'test.xml')
-
-#query = f'[out:json];node({llLat},{llLon},{urLat},{urLon});out;'
-
-#api = overpy.Overpass()
-#result = api.query(query)
-#json = result.to_json()
+outfile = os.path.join(os.getcwd(),'../sampleData/test.xml')
 
 if not os.path.isfile(outfile):
     # download the data using wget in a subprocess
