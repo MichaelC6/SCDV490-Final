3b3962c27e162527eda6f902e69f0257bfad0459
Noah Franz WSL <noahfranz13@gmail.com>
2023-03-28 23:57:21 -0400

    calculate optimal distance between chargers

--- findOptimalChargingDistance.py
+++ findOptimalChargingDistance.py
@@ -7,13 +7,36 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_theme(context='talk', style='white', palette='Set1')
 
 # read in and clean the data
 # this data is from https://afdc.energy.gov/data/10963
-filepath = os.path.join(os.getcwd(), 'sampleData', 'EV_range_efficiency.csv')
+dir = os.path.join(os.getcwd(), 'sampleData')
+filepath = os.path.join(dir, 'EV_range_efficiency.csv')
 ranges = pd.read_csv(filepath, skiprows=2)
 
 ranges = ranges.dropna(how='all', axis=1)
 ranges = ranges.dropna(how='all', axis=0)
 
+
+
 # let's histogram all the ranges and add vertical lines at the quartiles
+fig, ax = plt.subplots()
+ax.hist(ranges.Range, bins=15, edgecolor='k')
+ax.set_ylabel('N')
+ax.set_xlabel('Range (Miles)')
+
+quantiles = ranges.Range.quantile([0.10,0.25,0.5,0.75,0.90])
+ax.axvline(quantiles.iloc[4], linestyle='--', color='k', label='3rd Quartile')
+ax.axvline(quantiles.iloc[2], linestyle=':', color='k', label='Median')
+ax.axvline(quantiles.iloc[1], linestyle='-.', color='k', label='1st Quartile')
+
+ax.legend()
+
+fig.savefig('range-hist.png', bbox_inches='tight', transparent=False)
+
+# write quartiles out to a file
+quantiles.to_csv(os.path.join(dir, 'range-quantiles.csv'))
+print(f'I would recommend using the 25% limit at {quantiles.iloc[1]} miles')
