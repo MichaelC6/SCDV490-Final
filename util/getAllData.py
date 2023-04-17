fc52911ceabb750cb05426d3d0e2c440c866fec7
Noah Franz WSL <noahfranz13@gmail.com>
2023-04-17 16:13:00 -0400

    change to using bunzip

--- util/getAllData.py
+++ util/getAllData.py
@@ -2,23 +2,37 @@
 from subprocess import run
 import wget
 import os
+import argparse
 
-path = os.getcwd()
+def main():
 
-allStates = ["alabama","arizona","arkansas","california","colorado","connecticut","delaware",
-             "district-of-columbia","florida","georgia","idaho","illinois","indiana","iowa",
-             "kansas","kentucky","louisiana","maine","maryland","massachusetts","michigan","minnesota",
-             "mississippi","missouri","montana","nebraska","nevada","new-hampshire","new-jersey","new-mexico",
-             "new-york","north-carolina","north-dakota","ohio","oklahoma","oregon","pennsylvania",
-             "rhode-island","south-carolina","south-dakota","tennessee","texas",
-             "utah","vermont","virginia","washington","west-virginia","wisconsin","wyoming"]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--outdir', help='output directory', default=os.getcwd())
+    args = parser.parse_args()
+    
+    path = args.outdir
 
-allStates = ["rhode-island"]
+    allStates = ["alabama","arizona","arkansas","california","colorado","connecticut","delaware",
+                 "district-of-columbia","florida","georgia","idaho","illinois","indiana","iowa",
+                 "kansas","kentucky","louisiana","maine","maryland","massachusetts","michigan","minnesota",
+                 "mississippi","missouri","montana","nebraska","nevada","new-hampshire","new-jersey","new-mexico",
+                 "new-york","north-carolina","north-dakota","ohio","oklahoma","oregon","pennsylvania",
+                 "rhode-island","south-carolina","south-dakota","tennessee","texas",
+                 "utah","vermont","virginia","washington","west-virginia","wisconsin","wyoming"]
 
-link = "https://download.geofabrik.de/north-america/us/"
+    allStates = ["rhode-island"]
 
-for state in allStates:
-    stateLink = link + state + "-latest.osm.bz2"
-    filePath = os.path.join(path, "data", state + "-latest.osm.bz2")
-    wget.download(stateLink,filePath)
-    run(f'bzip2 -d {filePath}')
+    link = "https://download.geofabrik.de/north-america/us/"
+
+    for state in allStates:
+        stateLink = link + state + "-latest.osm.bz2"
+        filePath = os.path.join(path, state + "-latest.osm.bz2")
+
+        if not os.path.exists(filePath):
+            wget.download(stateLink,filePath)
+
+        if not os.path.exists(filePath[:-4]):
+            run(f'bunzip {filePath}')
+
+if __name__ == '__main__':
+    main()
