1172ce22b82fa43550817b4dfdd15acd7ef50692
Noah Franz WSL <noahfranz13@gmail.com>
2023-04-18 06:15:25 -0400

    update file reading for better searching

--- classes/search.py
+++ classes/search.py
@@ -1,6 +1,7 @@
 '''
 Class to hold software for the search algorithm. First attempt is DF
 '''
+import os, ast
 from util.preProcessing import readXML
 from util.mapFunctions import geodesicDist
 import numpy as np
@@ -16,29 +17,25 @@ class Search():
 
         self.filepath = filepath
 
+        outpath = os.path.join(os.path.split(self.filepath)[0], f'{stateName}-preprocessed.json')
         # read in the file with our XML parsing code
-        self.data = readXML(self.filepath, stateName, mp)
-
-        print(self.data)
-        
+        if os.path.exists(outpath):
+            print('Output file exists, reading in the data!') 
+            self.data = pd.read_json(outpath)
+        else:
+            print('Output file does not exist, cleaning the input file')
+            self.data = readXML(self.filepath, outpath, mp)
+            
         self.goodLocs = []
         
     def getRelevantNodes(self):
         '''
         Gets the relevant rows from self.data for the search
-
-        FIX ME! WILL NEED TO TEST WITH LARGER DATASET
         '''
-
-        goodRows = []
-        for ii,row in self.data.iterrows():
-            tag = row.tags
-            if len(tag.keys) > 0: print(tag.keys)
-            if 'amenity' in tag.keys:
-                print(tag.keys, tag.values)
+        self.data['hasAmenity'] = ['amenity' in row.tagKeys for ii,row in self.data.iterrows()]
+        self.data = self.data[self.data.hasAmenity == True].reset_index(drop=True)  
+        print(self.data)
         
-        
-
     def search(self, start=None, d=83.5, tol=1):
         '''
         Performs a search of nodes to find ideal locations of EV chargers
@@ -50,7 +47,7 @@ class Search():
         return : DataFrame of ideal EV charger locations
         '''
 
-        #self.getRelevantNodes()
+        self.getRelevantNodes()
 
         # get random starting location if start is None
         if start is None:
@@ -65,7 +62,7 @@ class Search():
         
         while True:
             #print(type(curr.to_frame()), curr.to_frame())
-            self.goodLocs.append(curr.to_frame().T)
+            self.goodLocs.append(curr.to_frame().transpose())
 
             # remove row from input dataframe to prevent loops
             locs = locs.drop(index=idx).reset_index(drop=True)
