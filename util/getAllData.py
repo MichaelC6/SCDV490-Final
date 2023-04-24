887c47fb611aebbdf83185ec379abe3c4fdc29ce
MichaelC6 <michaelcammarere@gmail.com>
2023-04-24 11:42:57 -0400

    Fixed GetAllData.py

--- util/getAllData.py
+++ util/getAllData.py
@@ -8,22 +8,26 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--outdir', help='output directory', default=os.getcwd())
+    parser.add_argument('--state', help='download one state', default=None)
     args = parser.parse_args()
     
     path = args.outdir
+    specificState = args.state
 
-    allStates = ["alabama","arizona","arkansas","california","colorado","connecticut","delaware",
-                 "district-of-columbia","florida","georgia","idaho","illinois","indiana","iowa",
-                 "kansas","kentucky","louisiana","maine","maryland","massachusetts","michigan","minnesota",
-                 "mississippi","missouri","montana","nebraska","nevada","new-hampshire","new-jersey","new-mexico",
-                 "new-york","north-carolina","north-dakota","ohio","oklahoma","oregon","pennsylvania",
-                 "rhode-island","south-carolina","south-dakota","tennessee","texas",
-                 "utah","vermont","virginia","washington","west-virginia","wisconsin","wyoming"]
+    if specificState == None:
 
-    allStates = ["new-york"]
+        allStates = ["alabama","arizona","arkansas","california","colorado","connecticut","delaware",
+                    "district-of-columbia","florida","georgia","idaho","illinois","indiana","iowa",
+                    "kansas","kentucky","louisiana","maine","maryland","massachusetts","michigan","minnesota",
+                    "mississippi","missouri","montana","nebraska","nevada","new-hampshire","new-jersey","new-mexico",
+                    "new-york","north-carolina","north-dakota","ohio","oklahoma","oregon","pennsylvania",
+                    "rhode-island","south-carolina","south-dakota","tennessee","texas",
+                    "utah","vermont","virginia","washington","west-virginia","wisconsin","wyoming"]
+
+    allStates = [specificState]
 
     link = "https://download.geofabrik.de/north-america/us/"
-
+    
     for state in allStates:
         stateLink = link + state + "-latest.osm.bz2"
         filePath = os.path.join(path, state + "-latest.osm.bz2")
